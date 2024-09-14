772970954084a1b25a0055f8576f35bfe956c4f4
Andreas Palm <andreaspalm98@gmail.com>
2024-09-14 16:23:19 +0200

    chore: Add test case for ListenAndConsume and Shutdown

--- consumer_test.go
+++ consumer_test.go
@@ -1,7 +1,12 @@
 package cone_test
 
 import (
+	"context"
+	"errors"
+	"fmt"
+	"sync/atomic"
 	"testing"
+	"time"
 
 	"github.com/zapling/cone"
 	"github.com/zapling/cone/conetest"
@@ -115,6 +120,128 @@ func TestServe(t *testing.T) {
 		c.Serve(r, conetest.NewEvent("is.wanted", nil))
 		if r.Result() != conetest.Ack {
 			t.Errorf("Expected %s but got: %s", conetest.Nak, r.Result())
+		}
+	})
+}
+
+func TestListenAndConsume(t *testing.T) {
+
+	t.Run("Calling func while still running should error", func(t *testing.T) {
+		source := conetest.NewSource()
+		c := cone.New(source)
+
+		go func() {
+			_ = c.ListenAndConsume()
+		}()
+
+		time.Sleep(1 * time.Millisecond) // Give first gorutine time to start
+
+		var returnErr error
+		go func() {
+			returnErr = c.ListenAndConsume()
+		}()
+
+		time.Sleep(5 * time.Millisecond)
+
+		if returnErr == nil {
+			t.Fatal("Expected error but got nil")
+		}
+	})
+
+	t.Run("Nil source should error", func(t *testing.T) {
+		c := cone.New(nil)
+		err := c.ListenAndConsume()
+		if err == nil {
+			t.Fatal("Expected error but got nil")
+		}
+	})
+
+	t.Run("Event should be served", func(t *testing.T) {
+		event := conetest.NewEvent("event.subject", nil)
+		source := conetest.NewSource()
+		source.AddEvent(event)
+
+		c := cone.New(source)
+		c.HandleFunc("event.subject", func(r cone.Response, _ cone.Event) {
+			_ = r.Ack()
+		})
+
+		go func() {
+			_ = c.ListenAndConsume()
+		}()
+
+		time.Sleep(5 * time.Millisecond)
+		if source.NumAckd() != 1 {
+			t.Error("Expected event to be acked, but it was not")
+		}
+	})
+}
+
+func TestShutdown(t *testing.T) {
+	t.Run("Unstarted consumer should error", func(t *testing.T) {
+		c := cone.New(nil)
+		err := c.Shutdown(context.Background())
+		if err == nil {
+			t.Error("Expected error but got nil")
+		}
+	})
+
+	t.Run("Should wait for all started handles", func(t *testing.T) {
+		source := conetest.NewSource()
+		source.AddEvent(conetest.NewEvent("event.subject", nil))
+		c := cone.New(source)
+		c.HandleFunc("event.subject", func(r cone.Response, _ cone.Event) {
+			time.Sleep(1 * time.Second)
+			_ = r.Ack()
+		})
+
+		var consumerHasStopped atomic.Bool
+		go func() {
+			err := c.ListenAndConsume()
+			if !errors.Is(err, cone.ErrConsumerStopped) {
+				panic(fmt.Sprintf("Unexpected error: %s", err.Error()))
+			}
+			consumerHasStopped.Swap(true)
+		}()
+
+		time.Sleep(5 * time.Millisecond)
+
+		err := c.Shutdown(context.Background())
+		if err != nil {
+			t.Fatalf("Expected nil but got err: %s", err.Error())
+		}
+
+		if !consumerHasStopped.Load() {
+			t.Fatal("Consumer never stopped!")
+		}
+
+		numAcked := source.NumAckd()
+		if numAcked != 1 {
+			t.Fatalf("Expected 1 acked message, but got: %d", numAcked)
+		}
+	})
+
+	t.Run("Should stop eariler ListenAndConsume", func(t *testing.T) {
+		source := conetest.NewSource()
+		c := cone.New(source)
+
+		var consumerHasStopped atomic.Bool
+		go func() {
+			err := c.ListenAndConsume()
+			if !errors.Is(err, cone.ErrConsumerStopped) {
+				panic(fmt.Sprintf("Unexpected error: %s", err.Error()))
+			}
+			consumerHasStopped.Swap(true)
+		}()
+
+		time.Sleep(5 * time.Millisecond)
+		err := c.Shutdown(context.Background())
+		if err != nil {
+			t.Fatalf("Expected nil but got err: %s", err.Error())
+		}
+
+		if !consumerHasStopped.Load() {
+			t.Fatal("Consumer never stopped!")
 		}
 	})
 }
