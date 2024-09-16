4e78df26910cd9c7cea6cf7a148c0c459bf66c95
Andreas Palm <andreaspalm98@gmail.com>
2024-09-16 15:35:39 +0200

    fix(conetest): Incorrect cone.NewEvent() call

--- conetest/event.go
+++ conetest/event.go
@@ -1,17 +1,11 @@
 package conetest
 
 import (
-	"context"
-
 	"github.com/zapling/cone"
 )
 
 func NewEvent(subject string, body []byte) *cone.Event {
-	return NewEventWithContext(context.Background(), subject, body)
-}
-
-func NewEventWithContext(ctx context.Context, subject string, body []byte) *cone.Event {
-	event, err := cone.NewEventWithContext(ctx, subject, body)
+	event, err := cone.NewEvent(subject, body)
 	if err != nil {
 		panic(err)
 	}
