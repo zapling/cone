58d8714df7ddf2844e02c75ea0ca40691d5bca2f
Andreas Palm <andreaspalm98@gmail.com>
2024-09-17 14:56:48 +0200

    feat(cone): Add event context

--- conetest/event.go
+++ conetest/event.go
@@ -1,11 +1,17 @@
 package conetest
 
 import (
+	"context"
+
 	"github.com/zapling/cone"
 )
 
 func NewEvent(subject string, body []byte) *cone.Event {
-	event, err := cone.NewEvent(subject, body)
+	return NewEventWithContext(context.Background(), subject, body)
+}
+
+func NewEventWithContext(ctx context.Context, subject string, body []byte) *cone.Event {
+	event, err := cone.NewEventWithContext(ctx, subject, body)
 	if err != nil {
 		panic(err)
 	}
--- event.go
+++ event.go
@@ -1,13 +1,34 @@
 package cone
 
+import "context"
+
 func NewEvent(subject string, body []byte) (*Event, error) {
-	return &Event{Subject: subject, Body: body, Header: make(Header)}, nil
+	return NewEventWithContext(context.Background(), subject, body)
+}
+
+func NewEventWithContext(ctx context.Context, subject string, body []byte) (*Event, error) {
+	return &Event{Subject: subject, Body: body, Header: make(Header), ctx: ctx}, nil
 }
 
 type Event struct {
 	Subject string
 	Body    []byte
 	Header  Header
+	ctx     context.Context
+}
+
+func (e *Event) Context() context.Context {
+	return e.ctx
+}
+
+func (e *Event) WithContext(ctx context.Context) *Event {
+	if ctx == nil {
+		panic("nil context")
+	}
+	e2 := new(Event)
+	*e2 = *e
+	e2.ctx = ctx
+	return e2
 }
 
 type Header map[string][]string
